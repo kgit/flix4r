bacd0b235efcadeab277c38f16baa7491e75668d
Docunext <albert.lash@docunext.com>
2010-04-26 02:49:39 -0400

    updated to require active_support

--- example.rb
+++ example.rb
@@ -2,6 +2,7 @@ require 'flix4r'
 
 
 NetFlix::Credentials.config_file_name = 'netflix.yml'
-list = NetFlix::Title.search(:term => 'sneakers', :max_results => 2)
+puts NetFlix::Request.default_cache_options
+list = NetFlix::Title.search(:term => 'sneak', :max_results => 2)
 
 puts list.first.title
--- lib/flix4r.rb
+++ lib/flix4r.rb
@@ -1,21 +1,20 @@
 
 this_dir = File.dirname(__FILE__)
 
-  require 'rubygems'
-  require 'activesupport'
-  require 'open-uri'
-  require 'yaml'
-  require 'hmac-sha1'
-  require 'json'
-  require 'net/http'
-  require 'fileutils'
-  require 'nokogiri'
-  require 'crack'
-  require 'api_cache'
-  require File.join( this_dir, 'valuable' )
+require 'rubygems'
+require 'active_support'
+require 'open-uri'
+require 'yaml'
+require 'hmac-sha1'
+require 'json'
+require 'net/http'
+require 'fileutils'
+require 'nokogiri'
+require 'crack'
+require 'api_cache'
+require File.join( this_dir, 'valuable' )
 
-  builders = File.join( this_dir, 'net_flix', 'builders')
+builders = File.join( this_dir, 'net_flix', 'builders')
 
-  ActiveSupport::Dependencies.load_paths << this_dir
-  ActiveSupport::Dependencies.load_paths << builders 
-
+ActiveSupport::Dependencies.load_paths << this_dir
+ActiveSupport::Dependencies.load_paths << builders
